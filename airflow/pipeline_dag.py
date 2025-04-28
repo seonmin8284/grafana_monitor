94cc20ed5dacf665690d6ba088a57d1dea31b254
seonmin8284 <seonmin8284@naver.com>
2025-04-28 23:45:35 +0900

    renamed:    news_pipeline_dag.py -> airflow/pipeline_dag.py 	modified:   docker-compose.yml 	new file:   fastapi/server.py 	renamed:    kafka_news_producer.py -> kafka/kafka_producer.py 	new file:   prometheus/prometheus.yml 	renamed:    news_stream_processor.py -> spark/stream_processor.py

--- airflow/pipeline_dag.py
+++ airflow/pipeline_dag.py
@@ -2,19 +2,19 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
-with DAG('news_pipeline_dag',
+with DAG('pipeline_dag',
          start_date=datetime(2024, 1, 1),
          schedule='@hourly',
          catchup=False) as dag:
 
     kafka_task = BashOperator(
         task_id='start_kafka_producer',
-        bash_command='python3 /Users/l-20190029/Downloads/kafka/kafka_news_producer.py'
+        bash_command='python3 kafka\kafka_producer.py'
     )
 
     spark_task = BashOperator(
         task_id='start_spark_streaming',
-        bash_command='spark-submit /Users/l-20190029/Downloads/kafka/news_stream_processor.py'
+        bash_command='spark-submit spark\stream_processor.py'
     )
 
     kafka_task >> spark_task
